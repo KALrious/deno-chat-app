7ee7647cb45c9963e936c6dd132147303606acbc
Jean Gautier <jgautier@oui.sncf>
2020-05-26 14:20:51 +0200

    remove unsued login on server

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import { listenAndServe } from 'https://deno.land/std/http/server.ts';
 import { acceptWebSocket, acceptable } from "https://deno.land/std/ws/mod.ts";
-import { chat, login } from "./chat.ts";
+import { chat } from "./chat.ts";
 
 listenAndServe({port: 3000}, async (req) => {
     if(req.method === 'GET' && req.url === '/') {
